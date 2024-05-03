bcaca39d33d11dce271530c294f07f3929fa4cd1
Sherif Alaa <sherefalex34@gmail.com>
2024-05-03 22:23:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 2001;
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
+});
+
+//npm init -y
+//npm install express
+//npm install nodemon
